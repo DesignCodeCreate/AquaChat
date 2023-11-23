872e7064ee32cdd09656d8a6b8e669d01a5203d7
Owen Hoskin <hoskinowen7@gmail.com>
2023-11-23 13:06:18 +0000

    Messages fulll loading commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,26 +46,25 @@
 	}
 
 	async function changeRoom(roomId) {
-		
 		const room = client.getRoom(roomId);
 		if (!room) return;
-		currentRoomDetails.id = roomId;
-		currentRoomDetails.name = room.name;
-		currentRoomDetails.messages = [];
-		currentRoomDetails.members = {};
+		currentRoomDetails = {
+			id: roomId,
+			name: room.name,
+			messages: [],
+			members: {}
+		};
 		peopleTyping = {};
+		client.sendEvent(currentRoomDetails.id, "m.fully_read", { event_id: roomId }, "");
 
-		await fetchMembers(roomId);
 
-
-		for (const event of room.timeline) {
-			if (event.getType() != "m.room.message") continue;
-			
-			currentRoomDetails.messages[event.event.event_id] = { 
-				created_at: event.event.origin_server_ts,
+		for (const message of (await client.roomInitialSync(roomId)).messages.chunk) {
+			if (message.type != "m.room.message") continue;			
+			currentRoomDetails.messages[message.event_id] = { 
+				created_at: message.origin_server_ts,
 				room: room,
-				member: room.getMember(event.getSender()),
-				content: marked.parse(event.event.content.body) 
+				member: room.getMember(message.sender),
+				content: marked.parse(message.content.body)
 			};
 		}
 	}
@@ -80,9 +79,8 @@
 
 		for (let member of members) {
 			if (!member.userId) continue;
-			const memberUser = client.getUser(member.userId);
 			const presenceState = await client.getPresence(member.userId);
-			currentRoomDetails.members[member.userId] = { displayName: memberUser.displayName, presence: presenceState.presence };	
+			currentRoomDetails.members[member.userId] = { displayName: member.name, presence: presenceState.presence };	
 		}
 		
 		return { members: Object.values(currentRoomDetails.members) };
@@ -92,7 +90,8 @@
 		for (const room of client.getRooms()) {
 			rooms[room.roomId] = {
 				roomCreator: room.myUserId,
-				roomName: room.name
+				roomName: room.name,
+				unread: room.notificationCounts.total
 			};
 		}
 		rooms = rooms;
@@ -111,12 +110,12 @@
 		
 		client.on("Room.timeline", (event, room) => {
 			if (event.getType() != "m.room.message") return;
-			if (room.roomId != currentRoomDetails["id"]) return;
-			currentRoomDetails["messages"][event.event.event_id] = { 
+			if (room.roomId != currentRoomDetails.id) return;
+			currentRoomDetails.messages[event.event.event_id] = { 
 				created_at: event.event.origin_server_ts,
 				room: room,
 				member: room.getMember(event.getSender()),
-				content: marked.parse(event.event.content.body) 
+				content: marked.parse(event.event.content.body)
 			};
 		});
 
@@ -128,10 +127,12 @@
 			if (state != "PREPARED") return;
 			fetchRooms();
 		});
+
+		client.on("m.presence", (_event, member) => {
+			fetchMembers();
+		});
+
 		client.on("Room", () => fetchRooms());
-
-
-		//console.log((await client.roomInitialSync(currentRoomDetails["id"])));
 	});
 
 	function handleTypingUpdate(event) {
@@ -145,46 +146,47 @@
 			"msgtype": "m.text"
 		};
 
-		client.sendEvent(currentRoomDetails["id"], "m.room.message", content, "");
+		client.sendEvent(currentRoomDetails.id, "m.room.message", content, "");
 	}
 </script>
 
 <div class="flex flex-row grow space-x-8 h-full">
 	<!-- Room list -->
 	<div class="p-4 w-1/4 max-h-full bg-slate-600 overflow-y-auto flex-grow-0 flex-shrink-0">
-	  <h3 class="mb-4 font-semibold text-xl text-gray-400"> Your rooms </h3>
-	  <p class="text-sm text-gray-400">
-		{#if Object.keys(rooms).length == 0}
-		  Loading...
-		{:else}
-		  {#each Object.entries(rooms) as [eventId, eventData]}
-			<button on:click={() => changeRoom(eventId)}> {eventData.roomName} </button>
-			<button on:click={() => leaveRoom(eventId)}> ✕ </button>
-			<br /><br />
-		  {/each}
-		{/if}
-	  </p>
+		<h3 class="mb-4 font-semibold text-xl text-gray-400"> Your rooms </h3>
+		<p class="text-sm text-gray-400">
+			{#if Object.keys(rooms).length == 0}
+				Loading...
+			{:else}
+				{#each Object.entries(rooms) as [ eventId, eventData ]}
+					<button on:click={() => changeRoom(eventId)}> {eventData.roomName} </button>
+					<button on:click={() => leaveRoom(eventId)}> ✕ </button>
+					{eventData.unread}
+					<br /><br />
+				{/each}
+			{/if}
+		</p>
 	</div>
-  
+
 	<!-- Messages -->
 	<div class="flex flex-col grow mt-4 dark:text-white max-h-full overflow-y-auto flex-grow">
-	  {#if !currentRoomDetails["name"]}
-		Please select a room!
-	  {:else}
-		<h1 class="text-2xl"> Welcome to {currentRoomDetails["name"]}! </h1>
-		<div class="flex flex-col mt-2 grow w-full overflow-y-auto">
-		  {#each Object.values(currentRoomDetails["messages"]) as eventData}
-			<p>
-			  <b> {eventData.member.name} - {formatTime(eventData.created_at)} </b><br />
-			  {@html eventData.content}
-			</p>
-		  {/each}
-		</div>
-		<TextBox on:typing={handleTypingUpdate} on:updated={handleMessageUpdate} peopleTyping={peopleTyping} channel={currentRoomDetails["name"]} />
-	  {/if}
+		{#if !currentRoomDetails["name"]}
+			Please select a room!
+		{:else}
+			<h1 class="text-2xl"> Welcome to {currentRoomDetails["name"]}! </h1>
+			<div class="flex flex-col mt-2 grow w-full overflow-y-auto">
+				{#each Object.values(currentRoomDetails["messages"]) as eventData}
+					<p>
+						<b> {eventData.member.name} - {formatTime(eventData.created_at)} </b><br />
+						{@html eventData.content}
+					</p>
+				{/each}
+			</div>
+			<TextBox on:typing={handleTypingUpdate} on:updated={handleMessageUpdate} peopleTyping={peopleTyping} channel={currentRoomDetails["name"]} />
+		{/if}
 	</div>
-  
-		<!-- User list -->
+
+	<!-- User list -->
 	<div class="flex-shrink p-4 w-1/4 max-h-full bg-slate-600 overflow-y-auto flex-grow-0 flex-shrink-0">
 		<h3 class="mb-4 font-semibold text-xl text-gray-400"> Online - {Object.values(currentRoomDetails.members).filter(member => member.presence === "online").length} </h3>
 		<p class="text-sm text-gray-400">
@@ -210,5 +212,4 @@
 			</p>
 		{/if}
 	</div>
-
-  </div>
+</div>
